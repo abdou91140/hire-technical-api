91025a782684ad910a01ae5fac48b23ab9d576bd
abdou <abdoucisse91140@gmail.com>
2024-02-22 17:19:22 +0100

    first

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -1,22 +1,25 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
-
-use App\Entity\Project;
+use App\Form\ProjectType;
 use App\Service\ProjectManager;
+use Symfony\Component\Serializer\SerializerInterface;
 
-class ProjectController extends AbstractController {
+class ProjectController extends AbstractController
+{
 
     private $projectManager;
+    private $serializer;
 
-    public function __construct(ProjectManager $projectManager)
+    public function __construct(ProjectManager $projectManager, SerializerInterface $serializer)
     {
         $this->projectManager = $projectManager;
+        $this->serializer = $serializer;
     }
 
     /**
@@ -25,14 +28,15 @@ class ProjectController extends AbstractController {
     public function postProject(Request $request): JsonResponse
     {
         $project = $this->projectManager->createProject();
-        $form = $this->createFormBuilder($project)
-            ->add('name', TextType::class)
-            ->add('slug', TextType::class)
-            ->getForm();
-        $form->handleRequest($request);
+        $form = $this->createForm(ProjectType::class, $project);
+
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            return $this->json($project->serialize());
+            return JsonResponse::fromJsonString($this->serializer->serialize($project, 'json'));
         }
+
         return $this->json(["error" => "An error occured"]);
     }
 }
